15e64fff64257e833afe5bfcf9b73638b8d1ea82
Evan Saragih <evan@Evans-MacBook-Pro.local>
2026-07-14 08:25:28 +0700

    feat: add vercel basic auth middleware

---
+++ middleware.js
@@ -0,0 +1,31 @@
+export default function middleware(req) {
+  const authorizationHeader = req.headers.get('authorization');
+
+  if (authorizationHeader) {
+    const basicAuth = authorizationHeader.split(' ')[1];
+    const [user, password] = atob(basicAuth).split(':');
+
+    // 👇 This is the list of users you can manage
+    const allowedUsers = {
+      'admin': 'helix2024',
+      'recruiter': 'hireme'
+    };
+
+    // If the username exists and password matches, let them in
+    if (allowedUsers[user] === password) {
+      return new Response(null, {
+        headers: {
+          'x-middleware-next': '1' // Tell Vercel to show the Storybook
+        }
+      });
+    }
+  }
+
+  // Otherwise, show the password prompt
+  return new Response('Access Denied', {
+    status: 401,
+    headers: {
+      'WWW-Authenticate': 'Basic realm="Storybook"',
+    },
+  });
+}
